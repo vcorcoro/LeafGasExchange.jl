bddea6c843e3e3215d93abcba513060aa6aeb17f
vcorcoro <137559813+vcorcoro@users.noreply.github.com>
2026-01-22 09:32:37 -0800

    Add files via upload

--- src/cam.jl
+++ src/cam.jl
@@ -1,13 +1,14 @@
 # CAM photosynthesis model derived from Photo3 model - Hartzell, Bartlett, & Porporato (2018)
 
 @system CAMBase(CBase) begin
-    # parameters from Photo3
+    Tb: base_temperature => 20 ~ preserve(u"°C", parameter)
+    # parameters from Photo3 (O. ficus-indica)
     TH: high_temperature => 302.65 ~ preserve(parameter, u"K") 
     TW: low_temperature => 283.15 ~ preserve(parameter, u"K") 
-    TOPT => 288.65 ~ preserve(u"K")
+    TOPT => 288.65 ~ preserve(parameter, u"K") # shift to 28 for orchid
     Am_max: max_rate_of_malic_acid_storage_flux => 14 ~ preserve(parameter, u"μmol/m^2/s") # species dependent 
-    TR: relaxation_time => 90 ~ preserve(u"minute") # Relaxation time for circadian oscillator (min)
-    C0: parameter_for_decarboxylation_of_malic_acid => 3000 ~ preserve(u"μbar") # (umol/mol)
+    TR: relaxation_time => 90 ~ preserve(parameter, u"minute") # Relaxation time for circadian oscillator (min)
+    C0: parameter_for_decarboxylation_of_malic_acid => 3000 ~ preserve(parameter, u"μbar") # (umol/mol)
 
     Q: photosynthetic_active_radiation_conversion_factor => begin
         # 4.55 is a conversion factor from W to photons for solar radiation, Goudriaan and van Laar (1994)
@@ -35,9 +36,29 @@ end
 # coppied from C3, parameters adjusted for CAM based on Photo3
 # base temperature Tb = 20 instead of 25
 @system CAMc(CAMBase) begin
+    ### Coppied from C4 to account for CO2 fixation by PEPC
+    # Kp25: Michaelis constant for PEP caboxylase for CO2
+    Kp25: pep_carboxylase_constant_for_co2_at_25 => 80 ~ preserve(u"μbar", parameter)
+    Kp(Kp25, kTQ10): pep_carboxylase_constant_for_co2 => begin
+        Kp25 * kTQ10
+    end ~ track(u"μbar")
+
+    Vpm25: maximum_pep_carboxylation_rate_for_co2_at_25 => 70 ~ preserve(u"μmol/m^2/s" #= CO2 =#, parameter)
+    EaVp: activation_energy_for_pep_carboxylation => 75.1 ~ preserve(u"kJ/mol", parameter)
+    Vpmax(Vpm25, kT, EaVp, kN): maximum_pep_carboxylation_rate => begin
+        Vpm25 * kT(EaVp) * kN
+    end ~ track(u"μmol/m^2/s" #= CO2 =#)
+
+    # Assume PEP substrate is not limiting
+    Vp(Vpmax, Cm, Kp): pep_carboxylation_rate => begin
+        # PEP carboxylation rate, that is the rate of C4 acid generation
+        (Cm * Vpmax) / (Cm + Kp)
+    end ~ track(u"μmol/m^2/s" #= CO2 =#)
+    ###
+
     # 302 μmol/mol (at 293.2 K ~ 20.05 C) - Photo3
     Kc25: rubisco_constant_for_co2_at_25 => 302 ~ preserve(u"μbar", parameter)
-    # 59430 J/mol (at 293.2 K ~ 20.05 C) - Photo3
+    # 59430 J/mol (at 293.2 K ~ 20.05 C) - Photo3 (original reference)
     Eac: activation_energy_for_co2 => 59.43 ~ preserve(u"kJ/mol", parameter)
     Kc(Kc25, kT, Eac): rubisco_constant_for_co2 => begin
         Kc25 * kT(Eac)
@@ -60,7 +81,7 @@ end
     end ~ track(u"μbar")
 
     # VCMAX0 = 18. # maximum carboxylation capacity - Photo3
-    Vcm25: maximum_carboxylation_rate_at_25 => 18 ~ preserve(u"μmol/m^2/s" #= CO2 =#, parameter)
+    Vcm25: maximum_carboxylation_rate_at_25 => 18 ~ preserve(u"μmol/m^2/s" #= CO2 =#, parameter) # species dependent
     # HKC =  59430. # Activation Energy for Kc (J/mol) - Photo3
     EaVc: activation_energy_for_carboxylation => 52.43 ~ preserve(u"kJ/mol", parameter)
     Vcmax(Vcm25, kT, EaVc, kN): maximum_carboxylation_rate => begin
@@ -71,7 +92,11 @@ end
 # coppied from C3, parameters adjusted for CAM based on Photo3
 # base temperature Tb = 20 instead of 25
 @system CAMj(CAMBase) begin
-    # JMAX0 = 36. # maximum electron transport capacity - Photo3
+    # JMAX0 = 36. # maximum electron transport capacity - Photo3 (assume 2*Vcm25)
+    # JmVcm_ratio: ratio_of_Jm_to_Vcm => 2 ~ preserve(parameter)
+    # Jm25(JmVcm_ratio, Vcm25): maximum_electron_transport_rate_at_25 => begin
+    #     JmVcm_ratio*Vcm25 
+    # end ~ preserve(u"μmol/m^2/s" #= Electron =#, parameter)
     Jm25: maximum_electron_transport_rate_at_25 => 36 ~ preserve(u"μmol/m^2/s" #= Electron =#, parameter)
     # HAJ = 50000. # Activation Energy for Jmax (J/mol) - Photo3
     Eaj: activation_enthalpy_for_electron_transport => 50 ~ preserve(u"kJ/mol", parameter)
@@ -110,10 +135,10 @@ end
     
     # equations from PHoto 3
     # Photo3 uses Φ which is incoming radiaion (W/m^2), same as solrad
-    Rdv(Rd, solrad): dark_respiration_flux_to_calvin_cycle => begin
+    Rdv(Rd, solrad): dark_respiration_flux_to_vacuole => begin
         Rd * exp(-Cropbox.deunitfy(solrad))
     end ~ track(u"μmol/m^2/s")
-    Rdc(Rd, solrad): dark_respiration_flux_to_vacuole => begin
+    Rdc(Rd, solrad): dark_respiration_flux_to_calvin_cycle => begin
         Rd * (1 - exp(-Cropbox.deunitfy(solrad)))
     end ~ track(u"μmol/m^2/s")
 
@@ -133,20 +158,20 @@ end
 @system CircadianCycle(CAMBase) begin
    
     # Circadian oscillator constants
-    MU => .5 ~ preserve
-    BETA => 2.764 ~ preserve 
-    CIRC_1 => .365 ~ preserve
-    CIRC_2 => .55 ~ preserve
-    CIRC_3 => 10 ~ preserve
+    MU => .5 ~ preserve(parameter)
+    BETA => 2.764 ~ preserve(parameter) 
+    CIRC_1 => .365 ~ preserve(parameter)
+    CIRC_2 => .55 ~ preserve(parameter)
+    CIRC_3 => 10 ~ preserve(parameter)
     
-    α_1 => 0.01 ~ preserve
-    α_2 => 1/7 ~ preserve
-    K => .003 ~ preserve
+    α_1 => 0.01 ~ preserve(parameter)
+    α_2 => 1/7 ~ preserve(parameter)
+    K => .003 ~ preserve(parameter)
     
-    m0: initial_malic_acid_concentration => 0 ~ preserve(u"mol/m^3")
-    m_max: max_malic_acid_concentration => 230000000 ~ preserve(parameter, u"μmol/m^3") # species dependent
-    vcm => 0.0027 ~ preserve(parameter, u"m") # Value controlling relative storage of malate (m)
-    z0: initial_circadian_rhythm_order => 0.55 ~ preserve
+    m0: initial_malic_acid_concentration => 0 ~ preserve(parameter, u"mol/m^3")
+    m_max: max_malic_acid_concentration => 230 ~ preserve(parameter, u"mol/m^3") # species dependent
+    vcm => 0.0027 ~ preserve(parameter, u"m") # Value controlling relative storage of malate (m), volume to surface area ratio
+    z0: initial_circadian_rhythm_order => 0.55 ~ preserve(parameter)
     
     m_s(m_max,TH,Tk,TW,α_2): max_storage_concentration_of_malic_acid => begin
         m_max*((TH - Tk)/(TH - TW)*(1 - α_2) + α_2)
@@ -174,11 +199,13 @@ end
     
     m(Asv, Avc, Rdv, vcm): malic_acid_concentration => begin
         (Asv - Avc + Rdv)/vcm
-    end ~ accumulate(init=m0, min=0, u"μmol/m^3")
+    end ~ accumulate(init=m0, min=0, u"mol/m^3")
 
     z(m, m_e, m_max, TR): circadian_rhythm_order => begin
         (m - m_e)/(m_max*TR)
     end ~ accumulate(init=z0, min=0)
+
+    #add flag to track CAM phase, length of PhaseIII for calibration
 end
 
 @system CAMRate(CAMc, CAMj, CAMr, CircadianCycle) begin
@@ -211,13 +238,28 @@ end
     ###############
     # From Photo3 #
     ###############
+
+    #should this use Ad_cc or Ad_ci? - in Photo3 Ci is used here
+    # Asc(Ad_cc, Rdc, f_c, fΨv): co2_flux_stomata_to_calvin_cycle => begin
+    #     (Ad_cc - Rdc)*(1-f_c) * fΨv
+    # end ~ track(min=0, u"μmol/m^2/s")
     Asc(Ad_ci, Rdc, f_c, fΨv): co2_flux_stomata_to_calvin_cycle => begin
         (Ad_ci - Rdc)*(1-f_c) * fΨv
     end ~ track(min=0, u"μmol/m^2/s")
     
-    Asv(m_s,m,K,Tk,T,TOPT,Am_max,Rdv,f_m,fΨv): co2_flux_stomata_to_vacuole => begin
-        if m_s > m && 1 - K*(Cropbox.deunitfy(Tk - TOPT)^2) > 0
-            (Am_max*(1 - K*(Cropbox.deunitfy(Tk - TOPT))^2) - Rdv)*f_m * fΨv
+    # Km_m: michaelis_menten_coefficient_for_malic_acid => 200 ~ preserve(parameter,u"μbar")
+    # adjusted to incorporate response to CO2 (Cc - Γ)/(Cc + Km_m)
+    # Asv(m_s,m,K,Tk,T,TOPT,Am_max,Rdv,f_m,fΨv): co2_flux_stomata_to_vacuole => begin
+    #     if m_s > m && 1 - K*(Cropbox.deunitfy(Tk - TOPT)^2) > 0
+    #         (Am_max*(1 - K*(Cropbox.deunitfy(Tk - TOPT))^2) - Rdv)*f_m * fΨv
+    #     else
+    #         0
+    #     end
+    # end ~ track(u"μmol/m^2/s")
+
+    Asv(m_s,m,Vp,Rdv,f_m,fΨv): co2_flux_stomata_to_vacuole => begin
+        if m_s > m
+            (Vp - Rdv) * f_m * fΨv
         else
             0
         end
